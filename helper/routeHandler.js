37aaa11e9a664cfabe5243b0bb0bb7a4e19109f8
Adil-Ansar <ansari.adil2015@gmail.com>
2023-12-23 22:04:15 +0530

    Removed unnessary code

--- helper/routeHandler.js
+++ helper/routeHandler.js
@@ -2,14 +2,10 @@ const baseRouter = require("express").Router();
 
 const basePath = "/payment";
 
-const payerRoutes = require("../modules/payer/payerRoutes");
-const receiverRoutes = require("../modules/receiver/receiverRoutes");
 const userRoutes = require("../modules/user/userRoutes")
 const adminRoutes = require("../modules/admin/adminRoutes");
 const invoiceRoutes = require("../modules/invoice/invoiceRoutes")
 
-baseRouter.use("/payer", payerRoutes);
-baseRouter.use("/receiver", receiverRoutes);
 baseRouter.use("/admin", adminRoutes);
 baseRouter.use("/user", userRoutes);
 baseRouter.use("/invoice", invoiceRoutes);
