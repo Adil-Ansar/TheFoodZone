44d7b9d3195d5cf49ee3d27eb64f9df24025950e
adil <ansari.adil2015@gmail.com>
2024-04-11 23:36:00 +0530

    ok

--- modules/webListing/webListing.js
+++ modules/webListing/webListing.js
@@ -3,9 +3,11 @@ const { webListingModel } = require("../models/webListingModel");
 
 const webList = async (req, res) => {
     try {
-        const data = await webListingModel.find({}).exec();
+        const data = await webListingModel.findOne({}).exec();
         if (data) {
-            return data
+            return res.status(200).json({
+                data
+            });
         } else {
             return res.status(400).json({
                 meta: { msg: "Something went wrong.", status: false },
